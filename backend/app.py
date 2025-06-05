382ba3841f4e9a8bca741d59e8889efba2335d4d
2240joshua <jkuhn123@terpmail.umd.edu>
2025-06-04 20:59:36 -0400

    fixed cors

--- backend/app.py
+++ backend/app.py
@@ -31,7 +31,7 @@ def home():
 # 🌐 CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://foodbot-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
